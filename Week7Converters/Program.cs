a9726cc5557a50915aac6d14503317786b67ea9a
Opilane TTHK <opilane@tthk.ee>
2020-10-14 16:10:13 +0300

    wow

---
+++ Week7Converters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Transactions;
+
+namespace Week7Converters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter c to calculate fahrenheit to celcious, or f  to calculate celcious to fahrenheit.");
+            string userInput = Console.ReadLine();
+            if (userInput == "c")
+            {
+                ToCelcious();
+            }else
+            {
+                ToFahrenheit();
+            }
+        }
+
+        public static void ToCelcious()
+        {
+            Console.WriteLine("Enter the temperature in F: ");
+            int tempF = Int32.Parse(Console.ReadLine());
+            double result = (tempF - 32) * 5 / 9;
+            Console.WriteLine($"Result in Celcious: {result}");
+        }
+
+        public static void ToFahrenheit()
+        {
+            Console.WriteLine("Enter the temperature in C: ");
+            int tempC = Int32.Parse(Console.ReadLine());
+            double result = (tempC * 9) / 5 + 32;
+            Console.WriteLine($"Result in fahrenheit: {result}");
+        }
+    }
+}
